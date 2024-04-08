186882a45f79f1b85e9f6c43ca43e5ccc3e7a211
Akse1588 <155363614+Aksel588@users.noreply.github.com>
2024-04-08 23:29:14 +0400

    Add files via upload

---
+++ Shades of Pink/javascript/javascript.js
@@ -0,0 +1,9 @@
+const chat = document.getElementById('chat')
+
+chat.addEventListener('click', () => {
+    chat.style.width = '500px'
+    chat.style.height = '700px'
+})
+chat.addEventListener('click', () => {
+
+})
